2c96b3c825ee9039650a5a021dd40c1730611fa3
beingsham <Kishansham786@gmail.com>
2026-02-06 09:43:44 +0530

    Update api.js

--- frontend/hrms-frontend/src/api.js
+++ frontend/hrms-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://hrms-lite-6hbt.onrender.com",
 });
 
 // Add request interceptor to include auth token
